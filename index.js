8b6f0592e2b32d0dfa3eb4ad9a849009f50e8bc8
Duane <duanerichard47@hotmail.com>
2022-12-21 23:08:01 -0500

    commit tree efcdf1b53d2dfccb2ff627c9cdad06c30ac4e4f6

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1
+function add(a,b)
+{
+    return a +b;
+}
+//2
+function subtract (a,b)
+{
+        return a - b;
+}
+//3
+function multiply (a,b)
+{
+    return a * b;
+}
+//4
+function divide ( a,b )
+{
+    return a / b;
+}
+//5
+function increment (n)
+{
+    n++;
+    return n;
+}
+//6
+function decrement(n)
+{
+    n--;
+    return n;
+}
+//7
+function makeInt(n)
+{
+    const result = parseInt(n,10);
+    return result;
+}
+
+//8 
+function preserveDecimal(n)
+{
+   
+    const result = parseFloat(n);
+    return result;
+   
+}
